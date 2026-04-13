6686746f842f9ebad8e6508cf7797410b1791b7c
jhbai3 <jhbai3@iflytek.com>
2026-04-13 20:50:23 +0800

    fix: add remote image domains for Next.js Image component

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "**.xhscdn.com" },
+      { hostname: "**.baidu.com" },
+      { hostname: "**.supabase.co" },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
